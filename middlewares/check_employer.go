4b5d080d277323b30134adc5be84d83eceff6935
ArjunMalhotra07 <arjun03.malhotra@gmail.com>
2025-01-10 09:56:17 +0530

    changed check employer to gin

--- middlewares/check_employer.go
+++ middlewares/check_employer.go
@@ -1,40 +1,43 @@
 package middlewares
 
 import (
-	"context"
 	"fmt"
 	"net/http"
 
 	"github.com/ArjunMalhotra07/gorm_recruiter/constants"
-	"github.com/ArjunMalhotra07/gorm_recruiter/handlers"
 	"github.com/ArjunMalhotra07/gorm_recruiter/models"
 	"github.com/dgrijalva/jwt-go"
+	"github.com/gin-gonic/gin"
 )
 
-func CheckEmployer() func(http.Handler) http.Handler {
-	return func(next http.Handler) http.Handler {
-		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-			claimsInterface := r.Context().Value(constants.Claims)
-			if claimsInterface == nil {
-				response := models.Response{Message: "Unauthorized request", Status: http.StatusUnauthorized}
-				handlers.SendResponse(w, response, http.StatusUnauthorized)
-				return
-			}
-			claims, ok := claimsInterface.(jwt.MapClaims)
-			if !ok {
-				response := models.Response{Message: "Invalid claims format", Status: http.StatusUnauthorized}
-				handlers.SendResponse(w, response, http.StatusUnauthorized)
-				return
-			}
-			if isEmployer, exists := claims[constants.IsEmployer].(bool); !exists || !isEmployer {
-				response := models.Response{Message: "Not an employer!", Status: http.StatusUnauthorized}
-				handlers.SendResponse(w, response, http.StatusUnauthorized)
-				return
-			} else {
-				fmt.Println(claims[constants.IsEmployer].(bool))
-				r = r.WithContext(context.WithValue(r.Context(), "claims", claims))
-				next.ServeHTTP(w, r)
-			}
-		})
+func CheckEmployer() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		claimsInterface, exists := c.Get(constants.Claims)
+		if !exists {
+			response := models.Response{Message: "Unauthorized request", Status: http.StatusUnauthorized}
+			c.JSON(http.StatusUnauthorized, response)
+			c.Abort()
+			return
+		}
+		if claimsInterface == nil {
+			response := models.Response{Message: "Unauthorized request", Status: http.StatusUnauthorized}
+			c.JSON(http.StatusUnauthorized, response)
+			return
+		}
+		claims, ok := claimsInterface.(jwt.MapClaims)
+		if !ok {
+			response := models.Response{Message: "Invalid claims format", Status: http.StatusUnauthorized}
+			c.JSON(http.StatusUnauthorized, response)
+			return
+		}
+		if isEmployer, exists := claims[constants.IsEmployer].(bool); !exists || !isEmployer {
+			response := models.Response{Message: "Not an employer!", Status: http.StatusUnauthorized}
+			c.JSON(http.StatusUnauthorized, response)
+			return
+		} else {
+			fmt.Println(claims[constants.IsEmployer].(bool))
+			c.Set("claims", claims)
+			c.Next()
+		}
 	}
 }
