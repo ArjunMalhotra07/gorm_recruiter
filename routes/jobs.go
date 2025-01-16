2e5500358f0a8fbe947b90478ccb567714038163
ArjunMalhotra07 <arjun03.malhotra@gmail.com>
2025-01-16 13:57:47 +0530

    added job

--- routes/jobs.go
+++ routes/jobs.go
@@ -2,7 +2,9 @@ package routes
 
 import (
 	handlers "github.com/ArjunMalhotra07/gorm_recruiter/handlers/jobs"
+	"github.com/ArjunMalhotra07/gorm_recruiter/middlewares"
 	repo "github.com/ArjunMalhotra07/gorm_recruiter/repo/jobs"
+	"github.com/ArjunMalhotra07/gorm_recruiter/seeders"
 	"github.com/gin-gonic/gin"
 	"gorm.io/gorm"
 )
@@ -10,6 +12,7 @@ import (
 func JobRoutes(router *gin.RouterGroup, driver *gorm.DB) {
 	jobRepo := repo.NewJobRepo(driver)
 	jobHandler := handlers.NewJobHandler(jobRepo)
+	router.Use(middlewares.JwtVerify(seeders.JwtSecret))
 	router.POST("/apply/:job_id", jobHandler.ApplyToJob)
 	router.GET("/", jobHandler.GetAllJobs)
 	router.GET("/jobdata/:job_id", jobHandler.GetJobData)
