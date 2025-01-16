1a35d51465490dc683a982accc1bb5f8888d672a
ArjunMalhotra07 <arjun03.malhotra@gmail.com>
2025-01-16 13:42:38 +0530

    corrected employer routes

--- repo/employer/jobs_by_user.go
+++ repo/employer/jobs_by_user.go
@@ -1,9 +1,17 @@
 package repo
 
-import "github.com/ArjunMalhotra07/gorm_recruiter/models"
+import (
+	"log"
+	"strings"
+
+	"github.com/ArjunMalhotra07/gorm_recruiter/models"
+)
 
 func (r *EmployerRepo) GetJobsPostedByUser(userID string, isActive bool) ([]models.Job, error) {
 	var jobs []models.Job
-	err := r.Driver.Where("posted_by_id = ? AND is_active = ?", userID, isActive).Find(&jobs).Error
+	userID = strings.TrimSpace(userID)
+	err := r.Driver.Where("posted_by_id = ? AND is_active = ?", userID, true).Find(&jobs).Error
+	log.Printf("Fetched jobs: %v, Error: %v", jobs, err)
+	log.Printf("userID %v", userID)
 	return jobs, err
 }
