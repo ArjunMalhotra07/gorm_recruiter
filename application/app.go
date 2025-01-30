4f90e38b54c8481c92a960d5585ced2572585991
ArjunMalhotra07 <arjun03.malhotra@gmail.com>
2025-01-30 11:27:53 +0530

    extracted logic to application

--- application/app.go
+++ application/app.go
@@ -1,20 +1,40 @@
 package application
 
 import (
+	"log"
 	"net/http"
 
+	"github.com/ArjunMalhotra07/gorm_recruiter/bootstrap"
 	"github.com/ArjunMalhotra07/gorm_recruiter/models"
+	"github.com/ArjunMalhotra07/gorm_recruiter/pkg/config"
+	"github.com/ArjunMalhotra07/gorm_recruiter/pkg/db"
 	"github.com/ArjunMalhotra07/gorm_recruiter/routes"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
 	"gorm.io/gorm"
 )
 
-func New(driver *gorm.DB) *models.App {
+func NewApp(driver *gorm.DB) *models.App {
 	return &models.App{Router: routes.AppRoutes(driver), Driver: driver}
 }
 
-func StartServer(app *models.App) error {
+func StartServer() error {
+	cfg := config.NewConfig(".env")
+	sqlDB, err := db.NewMySQLDb(cfg)
+	if err != nil {
+		log.Fatalf("Database connection failed: %v", err)
+	}
+	if err := db.Migrate(sqlDB.DB); err != nil {
+		log.Fatalf("Database migration failed: %v", err)
+	}
+	app := NewApp(sqlDB.DB)
+	bootstrap.RegisterMetrics()
+	go func() {
+		http.Handle("/metrics", promhttp.Handler())
+		log.Println("Prometheus metrics available at /metrics")
+		log.Fatal(http.ListenAndServe(":9100", nil)) // Prometheus server runs on port 9100
+	}()
 	server := &http.Server{Addr: ":8080", Handler: app.Router}
-	err := http.ListenAndServe(server.Addr, server.Handler)
+	err = http.ListenAndServe(server.Addr, server.Handler)
 	/*
 		or
 		err := http.ListenAndServe(":8080", app.router)
--- main.go
+++ main.go
@@ -3,43 +3,17 @@ package main
 import (
 	"fmt"
 	"log"
-	"net/http"
 
 	"github.com/ArjunMalhotra07/gorm_recruiter/application"
-	"github.com/ArjunMalhotra07/gorm_recruiter/bootstrap"
-	"github.com/ArjunMalhotra07/gorm_recruiter/pkg/config"
-	"github.com/ArjunMalhotra07/gorm_recruiter/pkg/db"
 	"github.com/joho/godotenv"
-	"github.com/prometheus/client_golang/prometheus/promhttp"
 )
 
 func main() {
 	fmt.Println("Started main function")
-	// Load environment variables
 	if err := godotenv.Load(); err != nil {
 		log.Fatalf("Error loading .env file")
 	}
-	// Load configuration
-	cfg := config.NewConfig(".env")
-	// Initialize database
-	sqlDB, err := db.NewMySQLDb(cfg)
-	if err != nil {
-		log.Fatalf("Database connection failed: %v", err)
-	}
-	// Run database migrations
-	if err := db.Migrate(sqlDB.DB); err != nil {
-		log.Fatalf("Database migration failed: %v", err)
-	}
-	// Initialize application
-	app := application.New(sqlDB.DB)
-	bootstrap.RegisterMetrics()
-	go func() {
-		// Serve Prometheus metrics at /metrics endpoint
-		http.Handle("/metrics", promhttp.Handler())
-		log.Println("Prometheus metrics available at /metrics")
-		log.Fatal(http.ListenAndServe(":9100", nil)) // Prometheus server runs on port 9100
-	}()
-	if err := application.StartServer(app); err != nil {
+	if err := application.StartServer(); err != nil {
 		log.Fatal(err)
 	}
 }
