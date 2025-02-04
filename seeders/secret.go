f342bda358dd8acaf5be8622d25cbbd1a862b68c
ArjunMalhotra07 <arjun03.malhotra@gmail.com>
2025-02-04 14:54:39 +0530

    added seeders

---
+++ seeders/secret.go
@@ -0,0 +1,5 @@
+package seeders
+
+const JwtSecret string = "x_Witcher_Geralt_x"
+const PasswordHashingSecret string = "abc&1*~#^2^#s0^=)^^7%b34"
+const ResumeParserKey string = "c5ejZk6Sql1xlq9QamNQd79b7tyxbNxs"
